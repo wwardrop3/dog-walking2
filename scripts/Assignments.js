f5619fafa27ea93a5c06def6c10eed3ceae27034
Wil Wardrop <wwardrop@gmail.com>
2022-01-20 13:27:09 -0600

    Working code up to assessment.

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,36 @@
-import { getPets, getWalkers } from "./database.js"
+import { getCities, getPets, getWalkerCities, getWalkers } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const walkerCities = getWalkerCities()
+const cities = getCities()
+
+
+//define function that returns all walkerCities objects that correspond with the walker that is clicked on...should return array of matching objects
+
+export const getAllCities = (walker) => {
+    const allCities = []
+    for (const city of walkerCities) {
+        if (walker.id === city.walkerId) {
+            allCities.push(city.cityId)
+        }
+    }
+    return allCities
+}
+
+const getCityNames = (walkerCityArray) => {
+    const cityArray = []
+    for (const walkerCity of walkerCityArray) {
+        for (const city of cities) {
+            if (walkerCity === city.id) {
+                cityArray.push(` ${city.name}`)
+            }
+        }
+    }
+    return cityArray
+
+}
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -24,10 +52,12 @@ export const Assignments = () => {
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
+        const allWalkerCities = getAllCities(currentPetWalker)
+        const walkerCityNames = getCityNames(allWalkerCities)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${walkerCityNames}
             </li>
         `
     }
@@ -36,4 +66,5 @@ export const Assignments = () => {
 
     return assignmentHTML
 }
+
 
--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,13 +1,13 @@
-import { getWalkers } from "./database.js"
+import { getCities } from "./database.js"
 
-const walkers = getWalkers()
 
+const cities = getCities()
 
 export const CityList = () => {
     let citiesHTML = "<ol>"
 
-    for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li>${city.name}</li>`
     }
 
     citiesHTML += "</ol>"
